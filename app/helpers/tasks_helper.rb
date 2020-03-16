ab805dd5288f4586eb847898343bca5f2d5573b9
thmasker <diego.p.hidalgo@gmail.com>
2020-03-16 13:20:03 +0100

    Update tasks views

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,20 +1,20 @@
 module TasksHelper
-  def priority_select(data)
-    case data
-    when 1
-      "High"
-    when 2
-      "Medium"
-    when 3
-      "Low"
-    end
-  end
+	def priority_select(data)
+		case data
+		when 1
+			"High"
+		when 2
+			"Medium"
+		when 3
+			"Low"
+		end
+	end
 
-  def done_check(data)
-    if data
-      "Yes"
-    else
-      "No"
-    end
-  end
+	def done_check(data)
+		if data
+			"Yes"
+		else
+			"No"
+		end
+	end
 end
