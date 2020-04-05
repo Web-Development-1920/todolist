232c3f23dba639c63c00ca43009ca86e94253d35
thmasker <diego.p.hidalgo@gmail.com>
2020-04-05 14:06:26 +0200

    Add Sign Up view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
     devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+    
     has_many :tasks, dependent: :destroy
     has_many :projects, dependent: :destroy
-
-    validates :username, presence: true
-    validates :password, presence: true
+    
+    validates :name, :surname, :username, :email, :encrypted_password, presence: true
 end
